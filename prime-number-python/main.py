9089f5a9ed6681d14a67c77a6be60688feb0472a
Ayman A <ayman.bacc@gmail.com>
2021-01-08 20:07:10 +0000

    Add python prime number checker

---
+++ prime-number-python/main.py
@@ -0,0 +1,26 @@
+def prime_checker(number):
+  is_prime = True 
+
+  for i in range(1, number -1):
+    if number % i == 0:
+       is_prime = False
+  if is_prime:
+    print("It's a prime number")
+  else:
+    print("It's not a prime number")
+
+#ask the user to enter a number and check using the prime checker function
+
+should_continue = True
+
+#create a while loop to keep asking the user to check another number
+while should_continue:
+  n = int(input("Check this number: "))
+  prime_checker(number=n)
+
+  option = input("Would you like to check another number, press 'y'").lower()
+
+  if option == "y":
+    should_continue
+  else: 
+    should_continue = False
