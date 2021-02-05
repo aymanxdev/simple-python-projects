f0fae211cab4617ad97d1b8295b400b347e894d3
Ayman A <ayman.bacc@gmail.com>
2021-02-05 22:54:23 +0000

    Initialise the main file for ping-pong game

---
+++ ping-pong-game/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+from turtle import Screen
+
+screen = Screen()
+screen.setup(800,600)
+screen.bgcolor("black")
+screen.title("Ping-Pong Game (Table Tennis)")
+
+
+
+
+
+
+
+screen.exitonclick()
