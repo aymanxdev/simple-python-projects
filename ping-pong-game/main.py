3b9d3a53fb40d9f19c81c175d29e8d0c16eb604b
Ayman A <ayman.bacc@gmail.com>
2021-02-06 01:35:07 +0000

    refactor code OOP, added a separate file for the paddles

--- ping-pong-game/main.py
+++ ping-pong-game/main.py
@@ -1,10 +1,32 @@
-from turtle import Turtle
+from paddle import Paddle
 from turtle import Screen
 
 screen = Screen()
-screen.setup(800,600)
+
 screen.bgcolor("black")
+screen.setup(800, 600)
 screen.title("Ping-Pong Game (Table Tennis)")
+screen.listen()
+screen.tracer(0)
+
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+
+
+
+
 
 
 
