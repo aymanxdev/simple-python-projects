809109037ff8729a448f4c070dac94e5e80997d2
Ayman A <ayman.bacc@gmail.com>
2021-02-07 00:43:55 +0000

    added table tennis ball, and bouncing functionality for the Y axis

--- ping-pong-game/main.py
+++ ping-pong-game/main.py
@@ -1,5 +1,8 @@
 from paddle import Paddle
 from turtle import Screen
+from ball import Ball 
+
+import time
 
 screen = Screen()
 
@@ -12,18 +15,21 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
-
 screen.onkey(r_paddle.move_up, "Up")
 screen.onkey(r_paddle.move_down, "Down")
 screen.onkey(l_paddle.move_up, "w")
 screen.onkey(l_paddle.move_down, "s")
 
+pong_ball = Ball()
 game_is_on = True
 
 while game_is_on:
-    screen.update()
+  time.sleep(0.1)
+  screen.update()
+  pong_ball.move()
 
-
+  if pong_ball.ycor() > 280 or pong_ball.ycor() < -280:
+      pong_ball.bounce()
 
 
 
