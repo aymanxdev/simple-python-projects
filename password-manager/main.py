7cf08763d9bd6e2cc0e11bd04ccd1295a351c856
Ayman A <ayman.bacc@gmail.com>
2021-02-19 00:06:22 +0000

    add save password function to add data into a newly created txt file

--- password-manager/main.py
+++ password-manager/main.py
@@ -4,6 +4,15 @@ from PIL import Image, ImageTk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_to_file():
+    website = website_input.get()
+    email = username_input.get()
+    password = password_input.get()
+    print(f"{website} | {email} | {password}\n")
+    with open("data.txt","a") as passwords_data:
+        passwords_data.write(f"{website} | {email} | {password}\n")
+        website_input.delete(0, END)
+        password_input.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,11 +30,13 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website: ")
 website_label.grid(column= 0, row=1)
 website_input = Entry(width=36)
+website_input.focus()
 website_input.grid(column= 1, row=1, columnspan=2)
 # --------------- Email/Username Label --------------- #
 username_label = Label(text="Email/Username: ")
 username_label.grid(column= 0, row=2)
 username_input = Entry(width=36)
+username_input.insert(END, "ayman@email.com")
 username_input.grid(column= 1, row=2, columnspan=2)
 # --------------- Password Label --------------- #
 password_label = Label(text="Password: ")
@@ -36,7 +47,7 @@ password_input.grid(column= 1, row=3)
 generate_button = Button(text="Generate Password")
 generate_button.grid(column= 2, row=3)
 # --------------- Add Button --------------- #
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command= save_to_file)
 add_button.grid(column= 1, row= 4, columnspan=2)
 
 
